a6e4f7e9455cb0c1dc1b2ab07e3d0fadcfd366de
Arthur Phillip <artphil.dev@gmail.com>
2019-08-25 20:42:04 -0300

    update makefile and  change bool to char typ array

--- primes/parallel_primes.cpp
+++ primes/parallel_primes.cpp
@@ -8,7 +8,7 @@ int main(int argc, char **argv)
     long MAX_NUMBER;
     char OPTION;
     int N_THREADS, this_thread;
-    bool **all_numbers;
+    char **all_numbers;
     long number, multiple;
     clock_t start_time, end_time;
     bool to_print;
@@ -37,7 +37,7 @@ int main(int argc, char **argv)
     else if (OPTION == 'l')
         to_print = true;
 
-    all_numbers = (bool **)calloc(N_THREADS, sizeof(bool *));
+    all_numbers = (char **)calloc(N_THREADS, sizeof(char *));
 
     start_time = clock();
     wall_timer = omp_get_wtime();
@@ -45,8 +45,8 @@ int main(int argc, char **argv)
 
     for (this_thread = 0; this_thread < N_THREADS; this_thread++)
     {
-        all_numbers[this_thread] = (bool *)calloc(MAX_NUMBER, sizeof(bool));
-        all_numbers[this_thread][0] = all_numbers[this_thread][1] = true;
+        all_numbers[this_thread] = (char *)calloc(MAX_NUMBER, sizeof(char));
+        all_numbers[this_thread][0] = all_numbers[this_thread][1] = 1;
 
         t_timer[this_thread] = wall_timer;
     }
@@ -55,21 +55,23 @@ int main(int argc, char **argv)
     for (number = 2; number < MAX_NUMBER / 2; number++)
     {
         this_thread = omp_get_thread_num();
-        if (all_numbers[this_thread][number] == 0)
+        if (!all_numbers[this_thread][number])
         {
             for (multiple = number + number; multiple < MAX_NUMBER; multiple += number)
-                all_numbers[this_thread][multiple]=true;
+                all_numbers[this_thread][multiple] = 1;
         }
         // printf("Thread:%d number:%lu time:%lf\n", omp_get_thread_num(), number, omp_get_wtime() - t_timer[omp_get_thread_num()]);
         // t_timer[omp_get_thread_num()] = omp_get_wtime();
     }
 
+#pragma omp parallel for num_threads(N_THREADS) private(this_thread, number, multiple) schedule(static)
     for (number = 2; number < MAX_NUMBER; number++)
         for (this_thread = 1; this_thread < N_THREADS; this_thread++)
-           {
-               if(all_numbers[0][number]) break;
-               all_numbers[0][number] |= all_numbers[this_thread][number];
-           }
+        {
+            if (all_numbers[0][number])
+                break;
+            all_numbers[0][number] |= all_numbers[this_thread][number];
+        }
 
     end_time = clock();
 
@@ -77,7 +79,7 @@ int main(int argc, char **argv)
     {
         for (number = 2; number < MAX_NUMBER; number++)
         {
-            if (all_numbers[0][number] == false)
+            if (!all_numbers[0][number])
                 printf("%lu ", number);
         }
         printf("\n");
