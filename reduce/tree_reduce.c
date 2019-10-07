322075deb4cfa15bfe0c023f943b251a33f83ac8
artphil <artphil.dev@gmail.com>
2019-10-07 18:57:39 -0300

    tp3 update

--- reduce/tree_reduce.c
+++ reduce/tree_reduce.c
@@ -17,31 +17,82 @@ int main(int argc, char **argv)
 {
     int data_length;
     char option[5];
-    float *data, sum;
+    float *data, sum, *sum_aux, aux;
     int to_print, to_time;
-    int size, rank;
+    int i, j, n_procs, rank, count;
 
     struct timeval start_time, end_time;
 
+    // Le entrada
+    scanf(" %s", option);
+    printf("Lido : %s\n", option);
+    scanf(" %d", &data_length);
+    printf("Lido : %d\n", data_length);
+
     // Inicia programacao distribuida
     MPI_Init(&argc, &argv);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_size(MPI_COMM_WORLD, &n_procs);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
+    // Cria vetor auxiliar
+    count = (data_length / n_procs);
+    if (data_length % n_procs != 0)
+        count++;
+
+    sum_aux = (float *)calloc(count, sizeof(float));
+
+    j = 0;
     if (rank == 0)
     {
-        // Le entrada
-        scanf(" %s", option);
-        printf("Lido : %s\n", option);
-        scanf(" %d", &data_length);
-        printf("Lido : %d\n", data_length);
+        // Le dados
         data = (float *)calloc(data_length, sizeof(float));
-        for (int i = 0; i < data_length; i++)
+        for (i = 0; i < data_length; i++)
         {
             scanf(" %f", &data[i]);
             printf("Lido : %f\n", data[i]);
         }
 
+        // Inicia a contagem do tempo
+        gettimeofday(&start_time, NULL);
+
+        // Envia dados a partir do processo zero
+        for (i = 0; i < data_length; i++)
+        {
+            if (i % n_procs == 0)
+                sum_aux[j++] = data[i];
+            else
+                MPI_Send(&data[i], 1, MPI_FLOAT, i % n_procs, 0, MPI_COMM_WORLD);
+        }
+    }
+    else
+    {
+        // Recebe dados do processo zero
+        for (i = 0; i < count; i++)
+            MPI_Recv(&sum_aux[j++], 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+    }
+
+    sum = reduce(sum_aux, count);   // Retirar essa funcao
+
+    while (1)                       // Implementar arvore de reducao
+    {
+        break;
+    }
+
+    if (rank == 0)
+    {
+        for (i = 1; i < n_procs; i++)
+        {
+            MPI_Recv(&aux, 1, MPI_FLOAT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+            sum += aux;
+        }
+    }
+    else
+    {
+        MPI_Send(&sum, 1, MPI_FLOAT, 0, 0, MPI_COMM_WORLD);
+    }
+
+    if (rank == 0)
+    {
         // Identifica de impressao
         to_print = to_time = 0;
 
@@ -52,15 +103,6 @@ int main(int argc, char **argv)
         else if (option[0] == 's')
             to_print = 1;
 
-        // Inicia a contagem do tempo
-        gettimeofday(&start_time, NULL);
-    }
-
-    // Executa soma
-    sum = reduce(data, data_length);
-
-    if (rank == 0)
-    {
         // Encerra a contagem do tempo
         gettimeofday(&end_time, NULL);
 
